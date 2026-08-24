18f2416c9fbe78d72527b72a4100c79ff1f695b7
Luis Figueroa <luisffp.04@gmail.com>
2026-08-23 21:51:40 -0600

    Flexbox, Grid y componentes reutilizables en Home y About

---
+++ BibliotecaMVC/Models/InfoCard.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaMVC.Models
+{
+    public class InfoCard
+    {
+        public string Icono { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
